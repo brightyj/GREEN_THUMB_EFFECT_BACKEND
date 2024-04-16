ad2a258cc3d4616efae91e78cc2f0ac86a02d867
Brighty <brightyjnithin@gmail.com>
2024-04-16 10:52:45 +0530

    BACKEND COMMITTED

--- index.js
+++ index.js
@@ -1,157 +1,147 @@
-const port=4000;
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const jwt=require("jsonwebtoken");
-const multer=require( "multer" );
-const path=require("path");
-const cors=require("cors");
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use(cors({
-    origin:['https://green-thumb-effect-frontend-7mxw.onrender.com','http://localhost:4000']
+    origin: ['https://green-thumb-effect-frontend-7mxw.onrender.com', 'http://localhost:4000']
 }));
 
-//Database Connection With MongoDB
+// Database Connection With MongoDB
+mongoose.connect("mongodb+srv://BritzMongo:rumbelstiltskin417*@cluster0.72usb72.mongodb.net/greenthumb");
 
-mongoose.connect("mongodb+srv://BritzMongo:rumbelstiltskin417*@cluster0.72usb72.mongodb.net/greenthumb")
-
-//API Creation
-
+// API Creation
 app.get("/", (req, res) => {
-
-res.send("Express App is Running")
-})
-
-//Image storage Engine
-const storage=multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-const upload=multer({storage:storage})
-//Creating Upload Endpoint for Images
-app.use('/images',express.static('upload/images'))
-app.post("/upload",upload.single('product'),(req,res)=>{
-res.json({
-    success:1,
-    image_url:`https://green-thumb-effect-backend.onrender.com/upload/images/${req.file.filename}`
-
-})
-})
-// Schema for Creating Products
-const Product=mongoose.model("Product",{
-    id:{
-        type:Number,
-        required:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    
-    },
-    image:{
-        type:String,
-        required:true,
-
-    },
-    category:{
-        type:String,
-        required:true,
-    },
-    new_price:{
-        type:Number,
-        required:true,
-    },
-    old_price:{
-        type:Number,
-        required:true,
-    },
-    date:{
-        type:Date,
-        default:Date.now,
-    },
-    avilable:{
-        type:Boolean,
-        default:true,
-    },
-})
-
-app.post('/addproduct',async(req,res)=>{
-    let products=await Product.find({});
-    let id;
-    if(products.length>0)
-    {
-        let last_product_array=products.slice(-1);
-        let last_product=last_product_array[0];
-        id=last_product.id+1;
-    }
-    else
-    {
-        id=1;
-    
-    }
-const product=new Product({
-    
-    id:id,
-    name:req.body.name,
-    image:req.body.image,
-    category:req.body.category,
-    new_price:req.body.new_price,
-    old_price:req.body.old_price,
+    res.send("Express App is Running");
 });
-console.log(product);
-await product.save();
-console.log("Saved");
-res.json({
-    success:true,
-    name:req.body.name,
-})
-})
 
-//Creating API for deleting product
-app.post('/removeproduct',async(req,res)=>{
-    await Product.findOneAndDelete({id:req.body.id});
-    console.log("Removed")
+// Image storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for Images
+app.use('/images', express.static('upload/images'));
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `https://green-thumb-effect-backend.onrender.com/upload/images/${req.file.filename}`
+    });
+});
+
+// Schema for Creating Products
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+app.post('/addproduct', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if (products.length > 0) {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    } else {
+        id = 1;
+    }
+    const product = new Product({
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
     res.json({
         success: true,
-        name:req.body.name,
+        name: req.body.name,
+    });
+});
 
-    })
+// Creating API for deleting product
+app.post('/removeproduct', async (req, res) => {
+    await Product.findOneAndDelete({ id: req.body.id });
+    console.log("Removed");
+    res.json({
+        success: true,
+        name: req.body.name,
+    });
+});
 
-})
 // Creating API for getting all products
-app.get('/allproducts', async (req , res) =>{
-    let products=await Product.find({});
+app.get('/allproducts', async (req, res) => {
+    let products = await Product.find({});
     console.log("All Products Fetched");
     res.send(products);
-    
-})
-//user model
-const Users=mongoose.model('Users',{
-    name:{
-        type:String,
-        unique:true,
-    },
-    email:{
-        type:String,
-        unique:true
-    },
-    password:{
-        type:String,
-    },
-    cartData:{
-        type:Object,
-    },
-    date:{
-        type:Date,
-        default:Date.now,
-    }
-})
+});
 
- //Register new user
+// User model
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// Register new user
 app.post('/signup', async (req, res) => {
     console.log("Sign Up");
 
@@ -205,7 +195,7 @@ app.post('/signup', async (req, res) => {
     }
 });
 
-//creating endpoint for the user login
+// Endpoint for user login
 app.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -234,25 +224,23 @@ app.post('/login', async (req, res) => {
     }
 });
 
-//Creating Middleware to fetch user
-const fetchUser= async(req,res,next)=>{
-    const token=req.header('auth-token');
+// Middleware to fetch user
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
     console.log(token);
-    if( !token ) {
-     res.status(401).send({errors:"Please authenticate using valid token"})
-    }
-    else{
-        try{
-            const data=jwt.verify(token,'secret_greenthumb');
-        req.user=data.user;
-        next();
-        }catch(error){
-            res.status(401).send({errors:"Please aunthenticate using a valid token"})
-
+    if (!token) {
+        res.status(401).send({ errors: "Please authenticate using valid token" });
+    } else {
+        try {
+            const data = jwt.verify(token, 'secret_greenthumb');
+            req.user = data.user;
+            next();
+        } catch (error) {
+            res.status(401).send({ errors: "Please authenticate using a valid token" });
         }
     }
-}
-//creating endpoint for adding products in cartdata
+};
+
 // Endpoint for adding products to cart data
 app.post('/addtocart', fetchUser, async (req, res) => {
     console.log("Added", req.body.itemid);
@@ -272,9 +260,9 @@ app.post('/removefromcart', fetchUser, async (req, res) => {
     await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData }); 
     res.json({ success: true, message: "Item removed from cart" }); // Send JSON response
 });
-//creating endpoint to get cartdata
+
+// Endpoint to get cart data
 app.post('/getcart', fetchUser, async (req, res) => {
-   
     try {
         // Fetch the user's cart data from the database
         const userData = await Users.findOne({ _id: req.user.id });
@@ -285,14 +273,10 @@ app.post('/getcart', fetchUser, async (req, res) => {
     }
 });
 
-app.listen (port,(error)=>{
-    if(!error){
-    console.log(`Server is running on Port `+port)
-}
-else
-{
-console.log("Error:"+error)
-}
-})
-
-
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Server is running on Port ${port}`);
+    } else {
+        console.log("Error:" + error);
+    }
+});
